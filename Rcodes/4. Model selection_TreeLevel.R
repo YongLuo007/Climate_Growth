2b0473a4a8ca2dd5553e6fc95f5c2dd1e981909c
Yong Luo <yluo1@lakeheadu.ca>
2016-11-28 15:08:06 -0800

    final year models

--- Rcodes/4. Model selection_TreeLevel.R
+++ Rcodes/4. Model selection_TreeLevel.R
@@ -2,7 +2,7 @@ rm(list = ls())
 library(dplyr); library(SpaDES); library(nlme); library(data.table);library(MuMIn)
 library(parallel)
 workPath <- "~/GitHub/Climate_Growth"
-analysesData <- read.csv(file.path(workPath, "data", "newAllData.csv"), header = TRUE,
+analysesData <- read.csv(file.path(workPath, "data", "newAllDataRescaledComp.csv"), header = TRUE,
                          stringsAsFactors = FALSE) %>% data.table
 studySpecies <- c("All species", "Jack pine", "Trembling aspen", "Black spruce", "Other species")
 source(file.path(workPath, "Rcodes", "Rfunctions", "mixedModelSelection.R"))
@@ -18,38 +18,37 @@ for(indispecies in studySpecies){
                     logSBctd = log(PlotBiomass)-mean(log(PlotBiomass)))]
   tempoutput <- mixedModelSelection(DV = "logY", 
                                     IDV = c("logDBHctd", "Yearctd", "logIntraHctd",
-                                            "logInterHctd", "logSActd", "RBIctd"),
+                                            "logInterHctd", "logSActd"),
                                     maxInteraction = 2,
                                     ICTerm = "AIC",
                                     ICCut = 2,
                                     data = speciesData,
                                     random = ~1|PlotID/uniTreeID, 
                                     control = lmeControl(opt="optim", maxIter=50000, msMaxIter = 50000))
-  bestModel <- lme(fixed = as.formula(paste("logY~", paste(tempoutput$bestIDV, collapse = "+"))),
+  bestFormu <- as.formula(paste("logY~", paste(tempoutput$bestIDV, collapse = "+")))
+  bestModel <- lme(fixed = bestFormu,
                    data = speciesData,
-                   random = ~1|PlotID/uniTreeID, 
+                   random = ~1+Yearctd|PlotID/uniTreeID, 
                    control = lmeControl(opt="optim", maxIter=50000, msMaxIter = 50000))
   
   cat("Species", indispecies, "is done. \n")
   if(indispecies == "All species"){
     modelSelectionSummaries <- tempoutput$modelSummary[, Species:=indispecies]
-    bestFomula <- list(as.formula(paste("logY~", paste(tempoutput$bestIDV, collapse = "+"))))
+    bestFomula <- list(bestFormu)
     names(bestFomula) <- "All species"
     bestModels <- list(bestModel)
     names(bestModels) <- "All species"
   } else {
-    bestFomula[[indispecies]] <- as.formula(paste("logY~", paste(tempoutput$bestIDV, collapse = "+")))
+    bestFomula[[indispecies]] <- bestFormu
     modelSelectionSummaries <- rbind(modelSelectionSummaries, 
                                      tempoutput$modelSummary[, Species:=indispecies])
     bestModels[[indispecies]] <- bestModel
   }
 }
 
-  allFixedCoeff <- lapply(bestModels, function(x){
-    data.table(summary(x)$tTable, keep.rownames = TRUE)[, ':='(marR2 = r.squaredGLMM(x)[1],
-                                                               conR2 = r.squaredGLMM(x)[2])]})
-  
-
-
-
-
+allFixedCoeff <- lapply(bestModels, function(x){
+  data.table(summary(x)$tTable, keep.rownames = TRUE)[, ':='(marR2 = r.squaredGLMM(x)[1],
+                                                             conR2 = r.squaredGLMM(x)[2])]})
+rm(bestFormu, bestModel, getIC, getICformFomula, indispecies, mixedModelSelection, 
+   speciesData, tempoutput)
+save.image(file.path(workPath, "Results", "finalYearModels.RData"))
