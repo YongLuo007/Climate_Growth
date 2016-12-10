cbd84c6882d686bad0ae5b35b26bf52bd8a8258a
Yong Luo <yluo1@lakeheadu.ca>
2016-12-10 02:03:46 -0800

    figure 1

--- Rcodes/Figure 1 plot locations.R
+++ Rcodes/Figure 1 plot locations.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 library(data.table); library(ggplot2); library(SpaDES)
 library(nlme); library(dplyr);library(MuMIn); library(raster)
-library(maptools); library(rgeos)
+library(maptools); library(rgeos); library(colorRamps)
 
 workPath <- "~/GitHub/Climate_Growth"
 
@@ -20,6 +20,8 @@ ecozoneses <- c("Boreal Cordillera", "Boreal PLain",  "Boreal Shield",
 boreal <- ecozones[ecozones@data$ZONE_NAME %in% ecozoneses,]
 boreal <- spTransform(boreal, CRSobj = crs(canadamap))
 boreal <- gIntersection(boreal, canadamap)
+MBProvince <- canadamap[canadamap@data$NAME  == "Manitoba", ]
+studyArea <- gIntersection(boreal, MBProvince)
 
 df<- data.frame(id = "boreal")
 row.names(df) <- 1
@@ -76,7 +78,7 @@ canadamapall <- fortify(canadamap, region = "NAME") %>%
 canadamapall[,fill:=1]
 borealall <- fortify(boreal, region = "id") %>%
   data.table
-
+studyAreaall <- fortify(studyArea, region = "id") %>% data.table
 
 coordReferenceFinal <- data.table(coordReferenceFinal)[long <= max(canadamapall$long) &
                                                        long >= min(canadamapall$long) &
@@ -97,7 +99,7 @@ coordReferenceLabels[line %in% paste("long", seq(-130, -60, by = 10), sep = ""),
 coordReferenceLabels[line %in% paste("lat", seq(45, 60, by = 5), sep = ""),
                      labels := paste(seq(45, 60, by = 5), "N", sep = "")]
 
-figure1 <- ggplot(data = canadamapall, aes(x = long, y = lat)) +
+Figure1a <- ggplot(data = canadamapall, aes(x = long, y = lat)) +
   geom_polygon(aes(group = group, fill = as.factor(fill)))+
   scale_fill_manual(values = "gray")+
   geom_polygon(data = borealall, aes(x = long, y = lat, group = group),
@@ -107,7 +109,9 @@ figure1 <- ggplot(data = canadamapall, aes(x = long, y = lat)) +
             colour = "gray", linetype = 2)+
   geom_point(data = MBlocation,
              aes(x = MBlocation$Easting, y = MBlocation$Northing), col = "blue",
-             pch = 1, size = 2)+
+             pch = 1, size = 1.5)+
+  geom_polygon(data = studyAreaall, aes(x = long, y = lat, group = group), col = "red", 
+               alpha = 0, linetype = 2, size = 1)+
   geom_text(data = coordReferenceLabels, aes(x = long, y = lat, label = labels))+
   annotate("text", label = "BC", x = -1800000, y = 2000000, size = 5)+
   annotate("text", label = "AB", x = -1200000, y = 1700000, size = 5)+
@@ -116,6 +120,7 @@ figure1 <- ggplot(data = canadamapall, aes(x = long, y = lat)) +
   annotate("text", label = "ON", x = 600000, y = 1200000, size = 5)+
   annotate("text", label = "QC", x = 1500000, y = 1700000, size = 5)+
   annotate("text", label = "USA", x = -680000, y = 900000, size = 5)+
+  annotate("text", label = "a", x = -Inf, y = Inf, size = 8, vjust = 1.5, hjust = -1)+
   theme_bw()+
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -126,7 +131,104 @@ figure1 <- ggplot(data = canadamapall, aes(x = long, y = lat)) +
         legend.position = "none",
         legend.title = element_blank())
 
-ggsave(file = file.path(workPath, "TablesFigures", "Figure 1.png"), figure1, width = 14, height = 6)
+monthlyClimate <- read.csv(file.path(workPath, "data", "background_monthlyclimates.csv"), header = TRUE,
+                           stringsAsFactors = FALSE) %>% data.table
+names(monthlyClimate)[5:9] <- c("YearMonth", "Temperature", "Prep", "snowfall", "PET")
+monthlyClimate[,':='(Year = as.numeric(unlist(lapply(YearMonth, function(s) {unlist(strsplit(s, "/", fixed = T))[1]}))),
+                     Month = as.numeric(unlist(lapply(YearMonth, function(s) {unlist(strsplit(s, "/", fixed = T))[2]}))))]
+monthlyClimate[Month>=10, Year:=Year+1]
+annualClimate <- monthlyClimate[!(Year %in% c(1980, 2013)),.(Latitude = mean(Latitude), Longitude = mean(Longitude),
+                                   AnnualT = mean(Temperature), AnnualCMI = sum(Prep-PET)),
+                                by = c("ID", "Year")]
+IDs <- unique(annualClimate$ID)
+output <- data.table(ID = numeric(), Latitude = numeric(), Longitude = numeric(),
+                     TemperatureTrend = numeric(), CMITrend = numeric())
+for(id in IDs){
+  output <- rbind(output, 
+                  data.table(ID = id, Latitude = annualClimate[ID == id,]$Latitude[1],
+                             Longitude = annualClimate[ID == id,]$Longitude[1],
+                             TemperatureTrend = coef(lm(AnnualT~Year, data = annualClimate[ID == id,]))[2],
+                             CMITrend = coef(lm(AnnualCMI~Year, data = annualClimate[ID == id,]))[2]))
+}
+AnnualTRaster <- rasterFromXYZ(xyz = output[,.(Longitude, Latitude, TemperatureTrend)], crs = crs("+proj=longlat"))
+AnnualTRaster <- projectRaster(AnnualTRaster,
+                               res = c(0.05, 0.05),
+                               crs = crs(AnnualTRaster))
+AnnualTRaster <- projectRaster(AnnualTRaster, crs = crs(canadamap))
+AnnualTRaster <- mask(AnnualTRaster, studyArea)
+
+AnnualCMIRaster <- rasterFromXYZ(xyz = output[,.(Longitude, Latitude, CMITrend)], crs = crs("+proj=longlat"))
+AnnualCMIRaster <- projectRaster(AnnualCMIRaster,
+                               res = c(0.05, 0.05),
+                               crs = crs(AnnualCMIRaster))
+AnnualCMIRaster <- projectRaster(AnnualCMIRaster, crs = crs(canadamap))
+AnnualCMIRaster <- mask(AnnualCMIRaster, studyArea)
+
+AnnualTRaster_p <- rasterToPoints(AnnualTRaster)
+AnnualTRaster_p <- data.frame(AnnualTRaster_p)
+colnames(AnnualTRaster_p) <- c("long", "lat", "Temperature")
+cutpoints1 <- c(min(AnnualTRaster_p$Temperature), seq(0.005, 0.065, by = 0.01), max(AnnualTRaster_p$Temperature))
+AnnualTRaster_p$TemperatureClass <- cut(AnnualTRaster_p$Temperature, breaks = cutpoints1,
+                                        labels = seq(0, 0.07, by = 0.01), include.lowest = TRUE)
+
+
+AnnualCMIRaster_p <- rasterToPoints(AnnualCMIRaster)
+AnnualCMIRaster_p <- data.frame(AnnualCMIRaster_p)
+colnames(AnnualCMIRaster_p) <- c("long", "lat", "CMI")
+cutpoints2 <- seq(min(AnnualCMIRaster_p$CMI), max(AnnualCMIRaster_p$CMI), length = 9)
+labels2 <- c()
+for(i in 1:8){
+  labels2 <- c(labels2, (cutpoints2[i]+cutpoints2[i+1])/2)
+}
+AnnualCMIRaster_p$CMIClass <- cut(AnnualCMIRaster_p$CMI, breaks = cutpoints2,
+                                  labels = round(labels2, 2), include.lowest = TRUE)
+
+Figure1b <- ggplot(data = AnnualTRaster_p, aes(x = long, y = lat))+
+  geom_raster(aes(fill = Temperature))+
+  scale_fill_gradient2(name = expression(atop("Annual \ntemperature \ntrend", 
+                                         paste("(", degree, "C yea",r^{-1}, ")"))), low = "red", high = "blue")+
+  guides(fill = guide_colourbar(title.position = "top"))+
+  annotate("text", label = "b", x = -Inf, y = Inf, size = 8, vjust = 1.5, hjust = -1)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black", size = 1),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.title = element_text(size = 15),
+        legend.text = element_text(angle = 45, size = 12),
+        legend.direction = "horizontal",
+        legend.position = c(0.78, 0.19))
+
+Figure1c <- ggplot(data = AnnualCMIRaster_p, aes(x = long, y = lat))+
+  geom_raster(aes(fill = CMI))+
+  scale_fill_gradient2(name = expression(atop("Annual climate \nmoisture index \ntrend", 
+                                              paste("(mm yea",r^{-1}, ")"))),
+                       low = "red", high = "blue")+
+  guides(fill = guide_colourbar(title.position = "top"))+
+  annotate("text", label = "c", x = -Inf, y = Inf, size = 8, vjust = 1.5, hjust = -1)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black", size = 1),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.direction = "horizontal",
+        legend.title = element_text(size = 15),
+        legend.text = element_text(angle = 45, size = 12),
+        legend.position = c(0.78, 0.19))
+
+library(gridExtra)
+plotlayout <- rbind(c(1, 1), c(1, 1), c(2, 3), c(2, 3), c(2, 3))
+dev(4)
+a <- grid.arrange(Figure1a, Figure1b, Figure1c, layout_matrix = plotlayout)
+
+
+
+ggsave(file = file.path(workPath, "TablesFigures", "Figure 1.png"), 
+       a, width = 10, height = 10)
 
 
 
