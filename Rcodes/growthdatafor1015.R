5800b7b9fa871a9d58121277c0fc9277262c52a9
Yong Luo <yluo1@lakeheadu.ca>
2017-04-03 13:01:18 -0700

    derive Year 10 data

--- Rcodes/growthdatafor1015.R
+++ Rcodes/growthdatafor1015.R
@@ -11,22 +11,28 @@ selectionMethod <- "AllCensus_PositiveGrowth_RandomPlotADTree"
 analysesDataOrig <- fread(file.path(workPath, "data", selectionMethod, "finalData.csv"))
 analysesDataOrig <- analysesDataOrig[Species != "All species",]
 analysesDataOrig <- analysesDataOrig[order(uniTreeID, Year),]
+analysesDataOrig[ ,':='(MidYear = FinYear, 
+                        MidDBH = FinDBH, 
+                        MidFA = FinFA,
+                        MidBiomass = FinBiomass,
+                        MidBA = FinBA)]
 analysesDataOrig <- analysesDataOrig[,c("FinYear2", "FinDBH2", "FinFA2",  
                                         "FinBiomass2", "tempuniTreeID",
                                         "ATA2", "GSTA2", "NONGSTA2",
                                         "ACMIA2", "GSCMIA2",
-                                        "ACO2A2", "FinBA2") 
+                                        "ACO2A2", "FinBA2", "MidH") 
                                      := data.table::shift(x = analysesDataOrig[,.(FinYear, FinDBH, FinFA,
                                                                                   FinBiomass, uniTreeID,
                                                                                   ATA, GSTA, NONGSTA,
                                                                                   ACMIA, GSCMIA,
-                                                                                  ACO2A, FinBA)],
+                                                                                  ACO2A, FinBA, H)],
                                                           n = 1, fill = NA, type = "lead", give.names = FALSE)]
 analysesDataOrig <- analysesDataOrig[FinYear != FinYear2,]
 analysesDataOrig <- analysesDataOrig[tempuniTreeID == uniTreeID,][,tempuniTreeID := NULL]
 analysesDataOrig[, plotcensuslength:=length(unique(FinYear2)), by = PlotID]
 analysesDataOrig <- analysesDataOrig[plotcensuslength>1]
-analysesDataOrig[, BiomassGR2:=(FinBiomass2-IniBiomass)/10]
+analysesDataOrig[, ':='(BiomassGR2=(FinBiomass2-IniBiomass)/10,
+                        BAGR2 = (FinBA2-IniBA)/10)]
 analysesDataOrig <- rbind(data.table::copy(analysesDataOrig),
                           data.table::copy(analysesDataOrig)[,Species:="All species"])
 analysesDataOrig[,':='(FinYear = FinYear2,
@@ -34,6 +40,7 @@ analysesDataOrig[,':='(FinYear = FinYear2,
                        FinFA = FinFA2,
                        BiomassGR = BiomassGR2,
                        FinBA = FinBA2,
+                       BAGR = BAGR2,
                        Year = (IniYear+FinYear2)/2,
                        FinBiomass = FinBiomass2,
                        ATA = (ATA+ATA2)/2,
@@ -51,9 +58,21 @@ set(analysesDataOrig, , c("FinYear2", "FinDBH2", "FinFA2",
                           "NONGSPETA", "NONGSCMIA",
                           "GSCO2A", "NONGSCO2A", "positiveGrowthTree",
                           "FinBA2", "plotcensuslength",
-                          "BiomassGR2"), NULL)
-write.csv(analysesDataOrig, file.path(workPath, "data", 
-                                      selectionMethod, "finalData10.csv"),
+                          "BiomassGR2", "Year", "BAGR2"), NULL)
+analysesDataOrig <- analysesDataOrig[,.(PlotID, uniTreeID, allCensusLiveTree, Species,
+                                        species, IniYear, IniFA, IniDBH, IniH = H, IniBiomass, IniBA,
+                                        MidYear, MidFA, MidDBH, MidH, MidBiomass, MidBA,
+                                        FinYear, FinFA, FinDBH, FinBiomass, FinBA,
+                                        BiomassGR, BAGR,
+                                        ATA, GSTA, NONGSTA, ACMIA, GSCMIA, ACO2A)]
+analysesDataOrig[IniDBH == FinDBH, ':='(BiomassGR = 0,
+                                        BAGR = 0)]
+analysesDataOrig <- analysesDataOrig[order(Species, PlotID, uniTreeID, IniYear),]
+
+
+write.csv(analysesDataOrig, 
+          file.path(workPath, "data", 
+                    "Year10Analyses", "finalData10.csv"),
           row.names = F)
 
 
