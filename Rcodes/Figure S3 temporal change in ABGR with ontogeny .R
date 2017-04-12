539c46d739e85b854216335bdce924755c58f557
Yong Luo <yluo1@lakeheadu.ca>
2017-04-12 09:59:20 -0700

    figure S3

--- Rcodes/Figure S3 temporal change in ABGR with ontogeny .R
+++ Rcodes/Figure S3 temporal change in ABGR with ontogeny .R
@@ -3,14 +3,14 @@ rm(list = ls())
 library(data.table); library(ggplot2); library(SpaDES)
 library(nlme); library(dplyr);library(MuMIn);library(gridExtra)
 workPath <- "~/GitHub/Climate_Growth"
-selectionMethod <- "AllCensus_PositiveGrowth_RandomPlotADTree"
-load(file.path(workPath, "data", selectionMethod, "BestYearModels.RData"))
+selectionMethod <- "Year10Analyses"
+load(file.path(workPath, "data", selectionMethod, "FullYearModels.RData"))
 ##### for overall temporal trends and its dependency on DBH and RBI
-analysesData[,SA:=IniFA+2.5]
+analysesDataAll[,SA:=IniFA+2.5]
 ##### for overall temporal trends and its dependency on DBH and RBI
-for(i in 1:length(allHFixedCoeff)){
-  indicoeff <- allHFixedCoeff[[i]]
-  indicoeff[,Species:=names(allHFixedCoeff[i])]
+for(i in 1:length(fixedCoeffAll)){
+  indicoeff <- fixedCoeffAll[[i]]
+  indicoeff[,Species:=names(fixedCoeffAll[i])]
   if(i == 1){
     allHcoeff <- indicoeff
   } else {
@@ -28,65 +28,71 @@ output <- data.table(Species = character(), Direction = character(),
                      PredictedABGR_Lower = numeric(), PredictedABGR_Upper = numeric(),
                      overallSignificant = numeric())
 for(indispecies in studySpecies){
-  speciesData <- analysesData[Species == indispecies,]
+  speciesData <- analysesDataAll[Species == indispecies,]
   speciesallHcoeff <- allHcoeff[Species == indispecies, ]
-  allHbestFormula <- allHbestFormulas[[indispecies]]
-  theallHmodel <- allHbestModels[[indispecies]]
+  theallHmodel <- allfullModelsAll[[indispecies]]
+  minBiomass <- round(abs(min(speciesData$BiomassGR)), 3)+0.01
   # change with H
-  if(nrow(speciesallHcoeff[Direction == "changewithDBH",]) == 1){
-    changewithH <- data.table(expand.grid(Species = indispecies,
-                                          Direction = "changewithDBH",
-                                          Year = seq(min(speciesData$Year), 
-                                                     max(speciesData$Year),
-                                                     length = 100), 
-                                          DBH = exp(seq(log(min(speciesData$IniDBH)),
-                                                      log(max(speciesData$IniDBH)),
+  changewithH <- data.table(expand.grid(Species = indispecies,
+                                        Direction = "changewithDBH",
+                                        Year = seq(min(speciesData$IniYear), 
+                                                   max(speciesData$FinYear),
+                                                   length = 100), 
+                                        DBH = exp(seq(log(min(speciesData$IniDBH)),
+                                                      log(max(speciesData$FinDBH)),
                                                       length = 100)),
-                                          stringsAsFactors = FALSE))
-    changewithH[,':='(Yearctd = Year-mean(speciesData$Year),
-                      logDBHctd = log(DBH)-mean(log(speciesData$IniDBH)),
-                      logHctd = 0,
-                      logSActd = 0)]
-    changewithH[,CompetitionIntensity:=as.numeric(as.factor(DBH))]
-    fittedvalues <- predict(theallHmodel, newdata = changewithH, level = 0, se.fit = TRUE)
-    changewithH$PredictedABGR <- exp(fittedvalues$fit)
-    changewithH$PredictedABGR_Upper <- exp(fittedvalues$fit+1.98*fittedvalues$se.fit)
-    changewithH$PredictedABGR_Lower <- exp(fittedvalues$fit-1.98*fittedvalues$se.fit)
-    
-    output <- rbind(output, changewithH[,.(Species, Direction, Year, 
-                                           CompetitionIntensity,
-                                           PredictedABGR, PredictedABGR_Lower,
-                                           PredictedABGR_Upper,
-                                           overallSignificant = 0)])
-    rm(fittedvalues, changewithH)
+                                        stringsAsFactors = FALSE))
+  changewithH[,':='(Yearctd = Year-mean(speciesData$MidYear),
+                    logDBHctd = log(DBH)-mean(log(speciesData$MidDBH)),
+                    logHctd = 0,
+                    logSActd = 0)]
+  changewithH[,CompetitionIntensity:=as.numeric(as.factor(DBH))]
+  fittedvalues <- predict(theallHmodel, newdata = changewithH, level = 0, se.fit = TRUE)
+  changewithH$PredictedABGR <- exp(fittedvalues$fit)-minBiomass
+  changewithH$PredictedABGR_Upper <- exp(fittedvalues$fit+1.98*fittedvalues$se.fit)-minBiomass
+  changewithH$PredictedABGR_Lower <- exp(fittedvalues$fit-1.98*fittedvalues$se.fit)-minBiomass
+  outputaddon <- changewithH[,.(Species, Direction, Year, 
+                                CompetitionIntensity,
+                                PredictedABGR, PredictedABGR_Lower,
+                                PredictedABGR_Upper,
+                                overallSignificant = 1)]
+  if(speciesallHcoeff[Direction == "changewithDBH",]$`p-value`>=0.05){
+    outputaddon[, overallSignificant := 2]
   }
-  if(nrow(speciesallHcoeff[Direction == "changewithSA",]) == 1){
-    changewithH <- data.table(expand.grid(Species = indispecies,
-                                          Direction = "changewithSA",
-                                          Year = seq(min(speciesData$Year), 
-                                                     max(speciesData$Year),
-                                                     length = 100), 
-                                          SA = exp(seq(log(min(speciesData$SA)),
-                                                        log(max(speciesData$SA)),
-                                                        length = 100)),
-                                          stringsAsFactors = FALSE))
-    changewithH[,':='(Yearctd = Year-mean(speciesData$Year),
-                      logDBHctd = 0,
-                      logHctd = 0,
-                      logSActd = log(SA)-mean(log(speciesData$SA)))]
-    changewithH[,CompetitionIntensity:=as.numeric(as.factor(SA))]
-    fittedvalues <- predict(theallHmodel, newdata = changewithH, level = 0, se.fit = TRUE)
-    changewithH$PredictedABGR <- exp(fittedvalues$fit)
-    changewithH$PredictedABGR_Upper <- exp(fittedvalues$fit+1.98*fittedvalues$se.fit)
-    changewithH$PredictedABGR_Lower <- exp(fittedvalues$fit-1.98*fittedvalues$se.fit)
-    
-    output <- rbind(output, changewithH[,.(Species, Direction, Year, 
-                                           CompetitionIntensity,
-                                           PredictedABGR, PredictedABGR_Lower,
-                                           PredictedABGR_Upper,
-                                           overallSignificant = 0)])
-    rm(fittedvalues, changewithH)
+  output <- rbind(output, outputaddon)
+  
+  rm(fittedvalues, changewithH, outputaddon)
+  
+  
+  changewithH <- data.table(expand.grid(Species = indispecies,
+                                        Direction = "changewithSA",
+                                        Year = seq(min(speciesData$IniYear), 
+                                                   max(speciesData$FinYear),
+                                                   length = 100), 
+                                        SA = exp(seq(log(min(speciesData$IniFA)),
+                                                     log(max(speciesData$FinFA)),
+                                                     length = 100)),
+                                        stringsAsFactors = FALSE))
+  changewithH[,':='(Yearctd = Year-mean(speciesData$MidYear),
+                    logDBHctd = 0,
+                    logHctd = 0,
+                    logSActd = log(SA)-mean(log(speciesData$MidFA)))]
+  changewithH[,CompetitionIntensity:=as.numeric(as.factor(SA))]
+  fittedvalues <- predict(theallHmodel, newdata = changewithH, level = 0, se.fit = TRUE)
+  changewithH$PredictedABGR <- exp(fittedvalues$fit)-minBiomass
+  changewithH$PredictedABGR_Upper <- exp(fittedvalues$fit+1.98*fittedvalues$se.fit)-minBiomass
+  changewithH$PredictedABGR_Lower <- exp(fittedvalues$fit-1.98*fittedvalues$se.fit)-minBiomass
+  outputaddon <- changewithH[,.(Species, Direction, Year, 
+                                CompetitionIntensity,
+                                PredictedABGR, PredictedABGR_Lower,
+                                PredictedABGR_Upper,
+                                overallSignificant = 1)]
+  if(speciesallHcoeff[Direction == "changewithSA",]$`p-value`>=0.05){
+    outputaddon[, overallSignificant := 2]
   }
+  output <- rbind(output, outputaddon)
+  rm(fittedvalues, changewithH, outputaddon)
+  
 }
 
 output[Species == "Minor species", Species:="Minor species group"]
@@ -96,6 +102,7 @@ studySpecies <- c("All trees", studySpecies[2:4], "Minor species group")
 output[,':='(Species = factor(Species, studySpecies),
              Direction = factor(Direction, 
                                 levels = c("changewithDBH", "changewithSA")))]
+
 
 
 segmenttable <- data.table(Species = "All trees", 
@@ -123,12 +130,13 @@ texttable[,':='(Species = factor(Species,
                                    levels = c("changewithDBH", "changewithSA")))]
 texttable[,texts := letters[as.numeric(Direction)]]
 
-figure <- ggplot(data = output, aes(x = Year, y = PredictedABGR))+
+figure <- ggplot(data = output[overallSignificant == 1,], aes(x = Year, y = PredictedABGR))+
   facet_grid(Direction~Species, scale = "free_y",  drop = TRUE)+
   geom_line(aes(group = CompetitionIntensity, col = CompetitionIntensity), 
             size = 1)+
-  scale_y_continuous(name = expression(paste("Tree-level aboveground biomass growth rate (Kg ", year^{-1}, ")")))+
-  scale_x_continuous(name = "Year", breaks = seq(1990, 2010, by = 5))+
+  scale_y_continuous(name = expression(atop("Tree-level aboveground biomass growth rate",
+                                            paste("(Kg ", year^{-1}, " per tree)"))))+
+  scale_x_continuous(name = "Year", breaks = seq(1985, 2010, by = 5))+
   scale_color_continuous(name = "DBH / SA",
                          low = "blue", high = "red", breaks = c(3, 100),
                          labels = c("\nSmall / young", "Big / old\n"),
@@ -139,14 +147,14 @@ figure <- ggplot(data = output, aes(x = Year, y = PredictedABGR))+
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
-        axis.text = element_text(size = 13),
+        axis.text.y = element_text(size = 13),
+        axis.text.x = element_text(size = 13, angle = 45, vjust = 0.5),
         axis.title = element_text(size = 16),
         strip.text.y = element_blank(),
         strip.background = element_rect(colour = "white", fill = "white"),
         strip.text.x = element_text(size = 15, face = "italic"),
-        # legend.direction = "horizontal",
         legend.background = element_rect(colour = "black"),
-        legend.position = c(0.85, 0.35),
+        legend.position = c(0.90, 0.25),
         legend.text = element_text(size = 13),
         legend.title = element_text(size = 15))
 workPath <- "~/GitHub/Climate_Growth"
