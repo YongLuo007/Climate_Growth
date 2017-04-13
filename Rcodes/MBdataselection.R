e3b789730309f21f3697e81b605de1b76fff923c
Yong Luo <yluo1@lakeheadu.ca>
2017-04-13 09:50:03 -0700

    data selection for 10 years interval

--- Rcodes/MBdataselection.R
+++ Rcodes/MBdataselection.R
@@ -22,7 +22,7 @@ allPSP <- allPSP[StandType == "NAT",]
 print(length(unique(allPSP$PlotID))) # 271
 # 2. minimum 3 times
 allPSP[,measureTime:=length(unique(Year)), by = PlotID]
-allPSP <- allPSP[measureTime>=3,]
+allPSP <- allPSP[measureTime>=4,]
 print(length(unique(allPSP$PlotID))) # 172
 # 3. plot size bigger than 500m^2
 allPSP[,plotsizelength := length(unique(PlotSize)), by = PlotID]
@@ -52,58 +52,72 @@ allPSP[,':='(baseTreeAge = NULL, baseYear = NULL, baseFA = NULL)]
 allPSP[,':='(uniTreeID = paste(PlotID, "_", TreeNumber, sep = ""))]
 
 # save a file for calculating compeitition
-write.csv(allPSP, file.path(workPath, "data", "forcompetitionIndex.csv"), row.names = FALSE)
+write.csv(allPSP, file.path(workPath, "data", "Year10Analyses", "forcompetitionIndex.csv"), row.names = FALSE)
 
 
 
 ####$$$$%%%%%%%%%% tree level selection
 # 1. select all alive trees
-length(unique(allPSP$uniTreeID)) # 58034 trees
+length(unique(allPSP$uniTreeID)) # 43296 trees
 unique(allPSP$Status) #  1  2  4  0  9 NA  8
 allPSP[,':='(unhealthyTrees=max(Status)), by = uniTreeID]
 length(unique(allPSP[is.na(unhealthyTrees),]$uniTreeID)) # 20 trees that have NA status
 allPSP <- allPSP[!is.na(unhealthyTrees),]
-length(unique(allPSP$uniTreeID)) # 58014
-length(unique(allPSP[unhealthyTrees==2,]$uniTreeID)) # 4540 trees have physical damage
+length(unique(allPSP$uniTreeID)) # 43276
+length(unique(allPSP[unhealthyTrees==2,]$uniTreeID)) # 3471 trees have physical damage
 allPSP <- allPSP[unhealthyTrees!=2,]
-length(unique(allPSP$uniTreeID)) # 53474
+length(unique(allPSP$uniTreeID)) # 39805
 length(unique(allPSP[unhealthyTrees==4,]$uniTreeID)) # 10 trees have severe insect attack
 allPSP <- allPSP[unhealthyTrees!=4,]
-length(unique(allPSP$uniTreeID)) # 53464
+length(unique(allPSP$uniTreeID)) # 39795
 length(unique(allPSP[unhealthyTrees==9,]$uniTreeID)) # 360 trees have unknown causes of death
 allPSP <- allPSP[unhealthyTrees!=9,]
-length(unique(allPSP$uniTreeID)) # 53104
+length(unique(allPSP$uniTreeID)) # 39437
 length(unique(allPSP[unhealthyTrees==8,]$uniTreeID)) # 1 tree's death due to wind/snow 
 allPSP <- allPSP[unhealthyTrees!=8,]
-length(unique(allPSP$uniTreeID)) # 53103
+length(unique(allPSP$uniTreeID)) # 39436
 
 ###############################
-# there are 53103 trees
+# there are 39436 trees
 ###############################
 allPSP[,':='(firstPlotYear = min(Year), lastPlotYear = max(Year)), by = PlotID]
 allPSP[,':='(firstTreeYear = min(Year), lastTreeYear = max(Year)), by = uniTreeID]
 allPSP[firstPlotYear == firstTreeYear, RecruitMent := 0]
 allPSP[firstPlotYear != firstTreeYear, RecruitMent := 1]
-length(unique(allPSP[RecruitMent == 1,]$uniTreeID)) # 11 trees recruited (interesting)
+length(unique(allPSP[RecruitMent == 1,]$uniTreeID)) # 9 trees recruited (interesting)
 allPSP[lastPlotYear == lastTreeYear, Mortality := 0]
 allPSP[lastPlotYear != lastTreeYear, Mortality := 1]
-length(unique(allPSP[Mortality == 1,]$uniTreeID)) # 20845 trees (make sense)
+length(unique(allPSP[Mortality == 1,]$uniTreeID)) # 17292 trees (make sense)
 # 20627/52336 = 0.3941 
 # 20627/nrow(allPSP) = 0.11 ()
 
 
 allPSP <- allPSP[order(uniTreeID, Year),]
-allPSP <- allPSP[,c("FinYear", "FinDBH", "FinHeight",  "tempuniTreeID",
+shiftcol <- c("Year", "DBH", "Height", 
+              "uniTreeID", "FA")
+midcol <- c("MidYear", "MidDBH", "MidHeight",  "MiduniTreeID",
+            "MidFA") 
+
+fincol <- c("FinYear", "FinDBH", "FinHeight",  "tempuniTreeID",
                      "FinFA") 
-                  := data.table::shift(x = allPSP[,.(Year, DBH, Height, uniTreeID,
-                                         FA)],
-                           n = 1, fill = NA, type = "lead", give.names = FALSE)]
-allPSP <- allPSP[tempuniTreeID == uniTreeID,][,tempuniTreeID := NULL]
-allPSP <- allPSP[Year != FinYear,]
-length(unique(allPSP$uniTreeID)) # 46482
-simplePSP <- allPSP[,.(PlotID, uniTreeID, Species,IniYear = Year, IniFA = FA,
-                       IniDBH = DBH, IniHeight = Height, FinYear, 
-                       FinDBH, FinHeight, FinFA)]
+
+allPSP[, (midcol) := data.table::shift(x = .SD,
+                                       n = 1, fill = NA, type = "lead"), .SDcols = shiftcol,
+       by = uniTreeID]
+allPSP[, (fincol) := data.table::shift(x = .SD,
+                                       n = 2, fill = NA, type = "lead"), .SDcols = shiftcol,
+       by = uniTreeID]
+
+
+simplePSP <- allPSP[!is.na(tempuniTreeID), ][,c("MiduniTreeID", "tempuniTreeID") := NULL]
+
+length(unique(simplePSP$uniTreeID)) # 30295
+simplePSP <- simplePSP[,.(PlotID, uniTreeID, Species,IniYear = Year, IniFA = FA,
+                       IniDBH = DBH, IniHeight = Height, 
+                       MidYear, MidDBH, MidHeight, MidFA,
+                       FinYear, FinDBH, FinHeight, FinFA)]
+
+
 
 inputData <- data.table(data.frame(simplePSP))
 # "AS" 
@@ -136,11 +150,12 @@ inputData[Species == "WE",species:="white elm"]
 inputData[Species == "WS",species:="white spruce"]
 
 inputData[,length:=FinYear-IniYear]
-inputData <- inputData[length == 5,]
+
+inputData <- inputData[length == 10,]
 inputData[,measureLength:=length(unique(IniYear)), by = PlotID]
 inputData <- inputData[measureLength>=2,]
-unique(inputData$length) # 5
-length(unique(inputData$PlotID)) # 168 plots
+unique(inputData$length) # 10
+length(unique(inputData$PlotID)) # 125 plots
 # recruitment trees
 inputData[, ':='(plotfirstY = min(IniYear), plotlastY = max(FinYear)), by = PlotID]
 inputData[,':='(treefirstY = min(IniYear), treelastY = max(FinYear)), by = uniTreeID]
@@ -156,19 +171,22 @@ inputData[,positiveGrowthtemp := "no"]
 inputData[positiveGrowth == "yes" & lengthNegativeG == 1, positiveGrowthtemp := "yes"]
 
 inputData <- inputData[,.(PlotID, uniTreeID, allCensusLiveTree, 
-                          positiveGrowthTree = positiveGrowthtemp, Species,
-                          IniYear, IniFA, IniDBH, FinYear, FinDBH, FinFA, species)]
+                          positiveGrowthTree = positiveGrowthtemp, 
+                          Species_Org = Species, Species_Std = species,
+                          IniYear, IniFA, IniDBH, MidYear, MidFA, MidDBH, 
+                          FinYear, FinDBH, FinFA)]
 selectedplotsummary <- unique(inputData[,.(PlotID, IniYear, FinYear)], by = c("PlotID", "IniYear"))
-write.csv(selectedplotsummary, file.path(workPath, "data", "plotsummary.csv"),row.names=FALSE)
+write.csv(selectedplotsummary, file.path(workPath, "data", "Year10Analyses", "plotsummary.csv"),row.names=FALSE)
 
 
 selectedPSP <- allPSP[PlotID %in% unique(inputData$PlotID), ]
 selectedPSPMasterTable <- unique(selectedPSP[,.(PlotID, FMU, TWP, RGE, StandType, StructureType,
                                                 VegeType, VegeType, Easting, Northing)],
                                  by = "PlotID")
-write.csv(selectedPSPMasterTable, file.path(workPath, "data", "selectedPlotMasterTable.csv"),row.names=FALSE)
+write.csv(selectedPSPMasterTable, file.path(workPath, "data", "Year10Analyses", "selectedPlotMasterTable.csv"),row.names=FALSE)
 
-write.csv(inputData, file.path(workPath, "data","MBdataSimplified.csv"),
+write.csv(inputData, file.path(workPath, "data", "Year10Analyses",
+                               "MBdataSimplified.csv"),
           row.names = FALSE)
 
 
