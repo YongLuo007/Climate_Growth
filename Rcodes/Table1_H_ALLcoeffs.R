cca3924669ffd7c8e61f4b24cfec404941c1694e
Yong Luo <yluo1@lakeheadu.ca>
2017-05-15 16:14:32 -0700

    Table 1

--- Rcodes/Table1_H_ALLcoeffs.R
+++ Rcodes/Table1_H_ALLcoeffs.R
@@ -1,65 +1,39 @@
+# to produce relative importance figure
 rm(list = ls())
-library(dplyr); library(SpaDES); library(lme4); library(data.table);library(MuMIn)
-library(parallel)
-if(as.character(Sys.info()[6]) == "yonluo"){
-  workPath <- "~/Github/Climate_Growth"
-} else {
-  workPath <- file.path("", "home", "yonluo","Climate_Growth")
-}
+library(ggplot2, lib.loc = "~/GitHub/Climate_Growth/RequiredRPackages")
+library(SpaDES)
+library(data.table)
+
+
+
+workPath <- "~/GitHub/Climate_Growth"
 selectionMethod <- "Year10Analyses"
-analysesDataOrg <- fread(file.path(workPath, "data", selectionMethod, "finalData10.csv"))
-analysesDataAll <- analysesDataOrg[allCensusLiveTree == "yes",]
-allCoeffs <- data.frame(Variable = c("Intercept", "logDBH", "logSA", "logH", "Year",
-                                     "logDBH:logSA", "logDBH:logH", "logDBH:Year",
-                                     "logH:logSA", "logSA:Year",
-                                     "logH:Year"),
+load(file.path(workPath, "data",selectionMethod,
+               "FullYearModels.RData"))
+allCoeffs <- data.frame(Variable = c("(Intercept)", "DBH", "H",
+                                     "SA", "Year", 
+                                     "DBH:H", "DBH:SA",
+                                     "H:SA", "DBH:Year", 
+                                     "Year:H", "Year:SA"),
                         stringsAsFactors = FALSE)
-studySpecies <- c("All species", "Jack pine", "Trembling aspen", "Black spruce", "Minor species")
 
-for(indispecies in studySpecies){
-  speciesDataAll <- analysesDataAll[Species_Group == indispecies,]
-  minABGR <- round(abs(min(speciesDataAll$BiomassGR)), 3)+0.01
-  speciesDataAll[,':='(logY = log(BiomassGR+minABGR), 
-                       logDBHctd = log(MidDBH)-mean(log(MidDBH)), 
-                       Yearctd = MidYear-mean(MidYear),
-                       logHctd = log(H)-mean(log(H)),
-                       logSActd = log(MidFA)-mean(log(MidFA)))]
-  
-  fullModelAll <- lmer(logY~logDBHctd+Yearctd+logHctd+logSActd+
-                         logDBHctd:Yearctd+logDBHctd:logHctd+logDBHctd:logSActd+
-                         Yearctd:logHctd+Yearctd:logSActd+
-                         logHctd:logSActd+(Yearctd+1|PlotID/uniTreeID),
-                       data = speciesDataAll)
-  
-  
-  indiANOVA <- as.data.table(anova(fullModelAll),
-                             keep.rownames = TRUE)[,rn:=gsub("ctd", "", rn)]
-  indiANOVA <- indiANOVA[,.(Variable = rn, SS = round(`Sum Sq`, 2))]
-  indiANOVA[, Variable:=unlist(lapply(Variable,
-                                      function(s) 
-                                        paste(sort(unlist(strsplit(s, ":", fixed = TRUE))),
-                                              collapse = ":")))]
-  names(indiANOVA)[2] <- paste(indispecies, "_", names(indiANOVA)[2], sep = "")
-  indiCoeff <- as.data.table(summary(fullModelAll)$coefficients,
-                             keep.rownames = TRUE,
-                             stringsAsFactor = FALSE)[,rn:=gsub("ctd", "", rn)]
-  indiCoeff[rn=="(Intercept)", rn:="Intercept"]
-  indiCoeff <- indiCoeff[,.(Variable = rn, 
-                            Value = paste(round(Estimate, 4),
-                                          "(", round(`Std. Error`, 4),
-                                          ")", sep = ""))]
-  indiCoeff[, Variable:=unlist(lapply(Variable,
-                                      function(s) 
-                                        paste(sort(unlist(strsplit(s, ":", fixed = TRUE))), collapse = ":")))]
-  names(indiCoeff)[2] <- paste(indispecies, "_", names(indiCoeff)[2], sep = "")
-  allCoeffs <- dplyr::left_join(allCoeffs, indiCoeff, by = "Variable")
-  allCoeffs <- dplyr::left_join(allCoeffs, indiANOVA, by = "Variable")
-  cat("Species", indispecies, "is done. \n")
-  rm(fullModelAll)
+##### for overall temporal trends and its dependency on DBH and RBI
+for(i in 1:length(fixedCoeffAll)){
+  indicoeff <- fixedCoeffAll[[i]]
+  indicoeff[, Variable:= unlist(lapply(lapply(lapply((rn), function(x) unlist(strsplit(x, ":"))), function(y) sort(y)),
+                                       function(z) paste(z, collapse = ":")))]
+  indicoeff[, Variable:= gsub("ctd", "", rn)]
+  indicoeff[, Variable:= gsub("log", "", Variable)]
+  indicoeff <- indicoeff[,.(Variable, EstimateSE = paste(round(Value, 4), "(",
+                                                         round(`Std.Error`, 4), ")", sep = ""), 
+                            P = round(`p-value`, 2))]
+  names(indicoeff)[2:3] <- paste(names(fixedCoeffAll)[i], names(indicoeff)[2:3])
+  if(i == 1){
+    allcoeffs <- indicoeff
+  } else {
+    allcoeffs <- left_join(allcoeffs, indicoeff, by = "Variable")
+  }
 }
 
-
-
-workPath <- "~/Github/Climate_Growth"
-write.csv(allCoeffs, file.path(workPath, "TablesFigures", "Table1.csv"),
+write.csv(allcoeffs, file.path(workPath, "TablesFigures", "Table1.csv"),
           row.names = FALSE)
