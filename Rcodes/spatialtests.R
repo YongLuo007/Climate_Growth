d7508b15ebb9b97825489a651ddf64bfac258d17
Yong Luo <yluo1@lakeheadu.ca>
2017-06-30 12:23:28 -0700

    spatial test

--- Rcodes/spatialtests.R
+++ Rcodes/spatialtests.R
@@ -2,6 +2,9 @@ rm(list = ls())
 library(nlme)
 library(data.table)
 library(dplyr)
+library(sp)
+library(ade4)
+
 workPath <- "~/GitHub/Climate_Growth"
 selectionMethod <- "Year10Analyses"
 load(file.path(workPath, "data",selectionMethod,
@@ -10,33 +13,30 @@ load(file.path(workPath, "data",selectionMethod,
 
 allresiduals <- lapply(allfullModelsAll, function(s) summary(s)$data[, 
                                                      Residuals := as.numeric(residuals(s))])
-plotslope <- list()
-treeslope <- list()
+
+
+locations <- read.csv(file.path(workPath, "data", "selectedPlotMasterTable.csv"),
+                      header = TRUE,
+                      stringsAsFactors = FALSE) %>%
+  data.table
+locations <- locations[,.(PlotID, Zone = 14, Easting, Northing)] %>%
+  unique(., by = "PlotID")
+source(file.path(workPath, "Rcodes", "Rfunctions", "UTMtoLongLat.R"))
+locations <- UTMtoLongLat(locations, "+proj=longlat")$Transformed[,.(PlotID, Longitude, Latitude)]
+
+mantelTestResults <- data.table(Species = character(), P = numeric())
+
 for(indispecies in studySpecies){
-  if(indispecies == "All species"){
-    indimodel <- allfullModelsAll[[indispecies]]
-    indiplotslope <- data.table(random.effects(indimodel)$PlotID,
-                                keep.rownames = TRUE)
-  
-    indiplotslope[, PlotID := unlist(lapply(rn, function(s) unlist(strsplit(s, "/", fixed = TRUE))[2]))]
-    inditreeslope <- data.table(random.effects(indimodel)$uniTreeID,
-                                keep.rownames = TRUE)
-    inditreeslope[, PlotID := unlist(lapply(rn, function(s) unlist(strsplit(s, "/", fixed = TRUE))[2]))]
-    plotslope[[indispecies]] <- indiplotslope
-    treeslope[[indispecies]] <- inditreeslope
-  } else {
-    
-    indimodel <- allfullModelsAll[[indispecies]]
-    indiplotslope <- data.table(random.effects(indimodel)$PlotID,
-                                keep.rownames = TRUE)
-    
-    indiplotslope[, PlotID := unlist(lapply(rn, function(s) unlist(strsplit(s, "/", fixed = TRUE))[1]))]
-    inditreeslope <- data.table(random.effects(indimodel)$uniTreeID,
-                                keep.rownames = TRUE)
-    inditreeslope[, PlotID := unlist(lapply(rn, function(s) unlist(strsplit(s, "/", fixed = TRUE))[1]))]
-    plotslope[[indispecies]] <- indiplotslope
-    treeslope[[indispecies]] <- inditreeslope
-  }
+  speciesdata <- allresiduals[[indispecies]]
+  speciesdata <- speciesdata[,.(meanRedisuals = mean(Residuals)), by = PlotID]
+  speciesdata <- setkey(speciesdata, PlotID)[setkey(locations, PlotID), nomatch = 0]
+  plot_dists <- dist(cbind(speciesdata$Longitude,
+                           speciesdata$Latitude))
+  Redisual_dists <- dist(speciesdata$meanRedisuals)
+  a <- suppressWarnings(mantel.rtest(plot_dists, Redisual_dists, nrepet = 9999)$pvalue)
+  mantelTestResults <- rbind(mantelTestResults,
+                             data.table(Species = indispecies,
+                                        P = round(as.numeric(a), 2)))
 }
 
 
